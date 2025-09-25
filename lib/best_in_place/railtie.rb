c0bf76cfe42fd52cc9934ea611fcd785f07ed591
emmanu-snapmint <emmanu.varghese@snapmintfin.com>
2025-09-25 11:42:20 +0530

    fix for rails 6.1

--- lib/best_in_place/railtie.rb
+++ lib/best_in_place/railtie.rb
@@ -4,7 +4,16 @@ require 'action_view/base'
 module BestInPlace
   class Railtie < ::Rails::Railtie #:nodoc:
     config.after_initialize do
-      BestInPlace::ViewHelpers = ActionView::Base.new
+      if ActionView::Base.method(:new).arity == 3
+        # Rails 6+ requires: lookup_context, assigns, controller
+        lookup_context = ActionView::LookupContext.new(ActionController::Base.view_paths)
+        assigns = {}
+        controller = ActionController::Base.new
+        BestInPlace::ViewHelpers = ActionView::Base.new(lookup_context, assigns, controller)
+      else
+        # Older Rails versions
+        BestInPlace::ViewHelpers = ActionView::Base.new
+      end
     end
   end
 end
